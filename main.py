0c557b00a54825a2a143ea4e748fcd014f5a2139
洪学辰 <73264768+Hong-Xuechen@users.noreply.github.com>
2021-12-16 19:22:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
     text:str
         
 app = FastAPI()
-translator_en_to_zh = pipeline("sentiment-analysis")
+translator_en_to_zh = pipeline("translation_en_to_zh", "Helsinki-NLP/opus-mt-en-zh")
 
 @app.get("/")
 def root():
