d34d312169bfd9ca14d78df7a8d8f739bef3b330
洪学辰 <73264768+Hong-Xuechen@users.noreply.github.com>
2021-12-25 20:43:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
     text:str
         
 app = FastAPI()
-translator_en_to_zh = pipeline("text-generation")
+translator_en_to_zh = pipeline("translation_en_to_zh", "Helsinki-NLP/opus-mt-en-zh")
 
 @app.get("/")
 def root():
