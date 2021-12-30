eb0c81524720894b8863a833798b755d0cf9ba2a
洪学辰 <73264768+Hong-Xuechen@users.noreply.github.com>
2021-12-30 20:06:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from transformers import pipeline
 
 class Item(BaseModel):
     question:str
+    context:str
         
 app = FastAPI()
 nlp = pipeline("question-answering","distilbert-base-cased-distilled-squad")
-
-context = r"""
-Extractive Question Answering is the task of extracting an answer from a text given a question.
-"""
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
-@app.put("/question_answering")
+@app.post("/question_answering")
 def question_answering(item: Item):
-    return {"question_answering": nlp(question=item.question, context=context)}
+    return {"context": item.context,"question_answering": nlp(question=item.question, context=context)}
